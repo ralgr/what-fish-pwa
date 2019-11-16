b8a447104ca3b6ce14147e0c0826ba123e5ba0e9
ralgr <2torrk05@solent.ac.uk>
2019-11-16 13:08:43 +0000

    Update Firebase.js

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 var config = {
-    apiKey: "AIzaSyA2u1hJeE8US1Ox2fDMPrbkJcY2eO-MAhU",
-    authDomain: "emoji-app-tutorial-5c2df.firebaseapp.com",
-    databaseURL: "https://emoji-app-tutorial-5c2df.firebaseio.com",
-    projectId: "emoji-app-tutorial-5c2df",
-    storageBucket: "emoji-app-tutorial-5c2df.appspot.com",
-    messagingSenderId: "484436492762",
-    appId: "1:484436492762:web:42a96b5a9e5c162f"
+
   };
 
 export const fb = firebase.initializeApp(config);
